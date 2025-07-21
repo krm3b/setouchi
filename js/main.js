3c4687a774d41bcd45ad01f824a3a66a6c65f79d
yuoz <oz12iaev@gmail.com>
2025-07-21 23:35:33 +0900

    section voiceを作成しました

--- js/main.js
+++ js/main.js
@@ -12,6 +12,25 @@ $(function () {
 
   });
 
+
+  // ---------------------
+  // voice
+  // ---------------------
+  $(window).on('scroll', function () {
+    $('.item-left, .item-right').each(function () {
+      const targetPos = $(this).offset().top;
+      const scroll = $(window).scrollTop();
+      const windowHeight = $(window).height();
+
+      if (scroll > targetPos - windowHeight + 100) {
+        $(this).addClass('inview');
+      }
+    });
+  });
+
+  // ページ読み込み時にも発火（リロード直後に表示領域にある要素のため）
+  $(window).trigger('scroll');
+
 });
 
 
