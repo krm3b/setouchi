32f822e16052e28d7587d3d54ec3c3396f8ab8e8
yuoz <oz12iaev@gmail.com>
2025-07-22 22:49:58 +0900

    section eventを追加しました

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ $(function () {
     ハンバーガ―メニュー
   ===================================================*/
 
-
   /*=================================================
     スライドショー
   ===================================================*/
@@ -29,34 +28,49 @@ $(function () {
   /*=================================================
     Inview（画面に表示されたタイミングで処理を実行）
   ===================================================*/
-  $(window).scroll(function () {
+  $(window).scroll(function () {});
 
-
-  });
-
-
-  // ---------------------
+  // --------------------------------------------------
   // voice
-  // ---------------------
-  $(window).on('scroll', function () {
-    $('.item-left, .item-right').each(function () {
+  // --------------------------------------------------
+  $(window).on("scroll", function () {
+    $(".item-left, .item-right").each(function () {
       const targetPos = $(this).offset().top;
       const scroll = $(window).scrollTop();
       const windowHeight = $(window).height();
 
       if (scroll > targetPos - windowHeight + 100) {
-        $(this).addClass('inview');
+        $(this).addClass("inview");
       }
     });
   });
 
   // ページ読み込み時にも発火（リロード直後に表示領域にある要素のため）
-  $(window).trigger('scroll');
+  $(window).trigger("scroll");
 
+
+  
+  // ---------------------------------------------------
+
+
+
+  // ------------------------------------------------
+  // event
+  // ------------------------------------------------
+  $(".accordion .title").on("click", function () {
+    const $item = $(this).closest(".item");
+    const $content = $item.find(".content");
+    const $icon = $(this).find("span");
+
+    if ($content.is(":visible")) {
+      $content.slideUp();
+      $item.removeClass("open");
+      $icon.text("+");
+    } else {
+      $content.slideDown();
+      $item.addClass("open");
+      $icon.text("−");
+    }
+  });
+  // --------------------------------------------------
 });
-
-
-
-
-
-
